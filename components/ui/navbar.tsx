2fb98870cf3caf7b3df6932ad2632a8543b2b072
dickensodera <dickens@laevitas.ch>
2024-02-24 07:17:35 +0300

    update: Update navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,52 +1,168 @@
 'use client'
 
-//import { IonIcon, IonMenu } from "@ionic/react";
-import MenuIcon from '@mui/icons-material/Menu';
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import LogoMainTransparent from '../../app/assets/logo/logo-main-transparent-png-2.png'
-import Image from 'next/image'
+import logo from  '../../app/assets/logo/favicon.ico'
 
-export default function Navbar(){
-    const handleMenuClose = (e: any) => {
-        //e.preve
-        console.log(e)
-    }
-    return (
+const navigation = [
+  { name: 'Home', href: '#', current: true },
+  { name: 'About', href: '#', current: false },
+  { name: 'Features', href: '#', current: false },
+  { name: 'How it Works', href: '#', current: false },
+  { name: "Contact Us", href: "#", current: false}
+]
+
+function classNames(...classes:any) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Navbar() {
+  return (
+    <Disclosure as="nav" className="bg-[#010203]">
+      {({ open }) => (
         <>
-            <header 
-            //className="bg-gradient-to-t from-[#593690] to-[#9A1AAF]"
-            className="bg-[#010203] text-[#999999] w-[80%] mx-auto"
-            style={{
-                //background: 'radial-gradient(circle, rgba(34,193,195,1) 0%, rgba(198,45,253,1) 100%)'
-            }}
+          <div 
+            //className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8"
+            className='w-[80%] mx-auto px-2 sm:px-6 lg:px-8'
             >
-                <nav className="flex justify-between items-center w-[92%] mx-auto">
-                    <div className="platform-logo">
-                        {/* <h2 className="w-16">AjiraPay</h2> */}
-                        <img 
-                            className="w-16  text-white" 
-                            //src="https://cdn-icons-png.flaticon.com/512/5968/5968204.png" 
-                            src={LogoMainTransparent.src}
-                            alt="logo"
-                            //width={200}
-                            //height={100}
-                        />
-                        {/* <h2 className="w-16 text-white">AjiraPay</h2> */}
-                    </div>
-                   {/* <div className="md:static absolute md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
-                    <ul className="flex md:flex-row flex-col md:items-center md:gap-[2vw] gap-8 mobile:block">
-                            <li><a className="hover:text-gray-500" href="/">Home</a></li>
-                            <li><a className="hover:text-gray-500" href="#">About</a></li>
-                            <li><a className="hover:text-gray-500" href="#">Features</a></li>
-                            <li><a className="hover:text-gray-500" href="#">How it Works</a></li>
-                            <li><a className="hover:text-gray-500" href="#">Contact Us</a></li>
-                    </ul>
-                   </div>  */}
-                    <div className='flex items-center gap-6'>
-                        <button className="bg-[#00BF63] text-white px-5 py-2 rounded-md hover:bg-[#5B21B6] uppercase">Coming Soon</button>
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <img
+                    className="h-8 w-auto"
+                    //src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    src={logo.src}
+                    alt="Logo"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className='flex items-center gap-6'>
+                        <button className="bg-[#5B21B6] text-white px-5 py-2 rounded-md hover:bg-[#00BF63] uppercase">Coming Soon</button>
                         {/* <MenuIcon className="md:hidden text-3xl cursor-pointer " onClick={(e) => handleMenuClose(e)}></MenuIcon> */}
                     </div>
-                </nav>
-            </header>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              {/* <button className="bg-[#00BF63] text-white px-5 py-2 rounded-md hover:bg-[#5B21B6] uppercase">Coming Soon</button> */}
+                {/* <button
+                  type="button"
+                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">View notifications</span>
+                   <BellIcon className="h-6 w-6" aria-hidden="true" /> 
+                  
+                </button> */}
+
+                {/* Profile dropdown */}
+                {/* <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Your Profile
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Settings
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Sign out
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu> */}
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
         </>
-    )
+      )}
+    </Disclosure>
+  )
 }
