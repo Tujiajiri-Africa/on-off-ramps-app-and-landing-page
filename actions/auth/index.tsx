fc7fea58147ea532669470f956e0518989050f4b
dickensodera <dickens@laevitas.ch>
2024-03-02 21:50:08 +0300

    feat: consume external auth api

--- actions/auth/index.tsx
+++ actions/auth/index.tsx
@@ -4,11 +4,87 @@ import  * as z from 'zod'
 import { LoginSchema, RegisterSchema, PasswordResetSchema } from '@/schemas'
 
 export const register = async(values: z.infer<typeof RegisterSchema>) =>{
-    console.log(values)
+    const endpoint = "http://127.0.0.1:8000/api/v1/auth/register"
+
+    const validatedFields = RegisterSchema.safeParse(values)
+
+    if(!validatedFields.success){
+        return {error : 'Invalid credentials'}
+    }
+    
+    //const {email, password, confirm_password} = validatedFields.data
+
+    const payload = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(validatedFields.data)
+    }
+
+    try{
+        await fetch(endpoint,payload).then(async(response) =>{
+            if(response.status == 200 || response.status === 201){
+                const data = await response.json()
+                console.log(data)
+            }else if(response.status === 500){
+                console.log('server error while creating user')
+            }else if(response.status === 404){
+                console.log('Not found error')
+            }else if(response.status === 422){
+                console.log('validation error on server')
+            }
+          }).catch((error) => {
+                console.log(error)
+        })
+    }catch(error){
+        console.log(error)
+        throw error
+    }
 }
 
-export const login = async(values: z.infer<typeof LoginSchema>) => {
-    console.log(values)
+export const login = async(values: z.infer<typeof LoginSchema>) => { 
+    const endpoint = "http://127.0.0.1:8000/api/v1/auth/login"
+
+    const validatedFields = LoginSchema.safeParse(values)
+
+    if(!validatedFields.success){
+        return {error : 'Invalid credentials'}
+    }
+
+    //const {email, password} = validatedFields.data
+
+    const payload = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(validatedFields.data)
+    }
+
+    try{
+        await fetch(endpoint, payload)
+        .then(async(response) =>{
+            if(response.status === 200 || response.status === 201){
+                const data = await response.json()
+                const message = data['message']
+                console.log(data)
+            }else if(response.status === 500){
+                console.log('server error while logging in')
+            }else if(response.status === 404){
+                console.log('Not found error')
+            }else if(response.status === 422){
+                console.log('validation error on server')
+            }
+    
+          })
+          .catch((error) => {
+            console.log(error)
+        })
+    }catch(error){
+        console.log(error)
+        throw error
+    }
 }
 
 export const resetPassword = async(values: z.infer<typeof PasswordResetSchema>) =>{
