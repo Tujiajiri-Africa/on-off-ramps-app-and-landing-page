01661a75751c6941446dfc8c3e3be1a785d105fa
dickensodera <dickens@laevitas.ch>
2024-03-16 03:36:56 +0300

    update: Update recent transactions UI

--- components/recent-sales.tsx
+++ components/recent-sales.tsx
@@ -1,4 +1,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {Button} from '@/components/ui/button'
+import {PlusIcon, MinusIcon, Cross1Icon} from '@radix-ui/react-icons'
 
 export function RecentSales() {
   return (
@@ -6,61 +8,88 @@ export function RecentSales() {
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
           <AvatarImage src="/avatars/01.png" alt="Avatar" />
-          <AvatarFallback>OM</AvatarFallback>
+          <AvatarFallback>
+              <PlusIcon className="w-4 h-4"  />
+          </AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Olivia Martin</p>
-          <p className="text-sm text-muted-foreground">
-            olivia.martin@email.com
+          <p className="text-sm font-medium leading-none">Deposit</p>
+          <p className="text-sm  text-green-600"
+          //text-muted-foreground
+          >
+            Success
           </p>
         </div>
-        <div className="ml-auto font-medium">+$1,999.00</div>
+        <div className="ml-auto font-medium">KES 1,999.00</div>
       </div>
       <div className="flex items-center">
         <Avatar className="flex h-9 w-9 items-center justify-center space-y-0 border">
           <AvatarImage src="/avatars/02.png" alt="Avatar" />
-          <AvatarFallback>JL</AvatarFallback>
+          <AvatarFallback>
+            <PlusIcon className="w-4 h-4"  />
+          </AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Jackson Lee</p>
-          <p className="text-sm text-muted-foreground">jackson.lee@email.com</p>
+          <p className="text-sm font-medium leading-none">Buy USDT</p>
+          <p className="text-sm text-green-600"
+          //text-muted-foreground
+          >
+            Success
+            </p>
         </div>
-        <div className="ml-auto font-medium">+$39.00</div>
+        <div className="ml-auto font-medium">KES 39.00</div>
       </div>
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
           <AvatarImage src="/avatars/03.png" alt="Avatar" />
-          <AvatarFallback>IN</AvatarFallback>
+          <AvatarFallback>
+            <MinusIcon className="w-4 h-4"  />
+          </AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Isabella Nguyen</p>
-          <p className="text-sm text-muted-foreground">
-            isabella.nguyen@email.com
+          <p className="text-sm font-medium leading-none">Withdraw cUSD</p>
+          <p className="text-sm text-green-600"
+          //text-muted-foreground
+          >
+            Success
           </p>
         </div>
-        <div className="ml-auto font-medium">+$299.00</div>
+        <div className="ml-auto font-medium">KES 299.00</div>
       </div>
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
           <AvatarImage src="/avatars/04.png" alt="Avatar" />
-          <AvatarFallback>WK</AvatarFallback>
+          <AvatarFallback>
+            <PlusIcon className="w-4 h-4"  />
+          </AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">William Kim</p>
-          <p className="text-sm text-muted-foreground">will@email.com</p>
+          <p className="text-sm font-medium leading-none">Buy BTC</p>
+          <p className="text-sm text-green-600"
+          //text-muted-foreground
+            >
+              Success
+            </p>
         </div>
-        <div className="ml-auto font-medium">+$99.00</div>
+        <div className="ml-auto font-medium">KES 5,300.00</div>
       </div>
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
           <AvatarImage src="/avatars/05.png" alt="Avatar" />
-          <AvatarFallback>SD</AvatarFallback>
+          <AvatarFallback>
+            <Cross1Icon className="w-4 h-4" />
+          </AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Sofia Davis</p>
-          <p className="text-sm text-muted-foreground">sofia.davis@email.com</p>
+          <p className="text-sm font-medium leading-none">Sell USDC</p>
+          <p className="text-sm text-red-700">Insufficient Balance</p>
         </div>
-        <div className="ml-auto font-medium">+$39.00</div>
+        <div className="ml-auto font-medium">KES 3,039.00</div>
+      </div>
+      <div className="flex items-center">
+        <Button className=" bg-indigo-600 text-white dark:hover:bg-orange-600">
+          View More
+        </Button>
       </div>
     </div>
   );
