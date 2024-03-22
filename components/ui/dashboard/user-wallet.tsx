590793cc73d736405cd398684b6a1c8c2af522ed
dickensodera <dickens@laevitas.ch>
2024-03-22 08:00:03 +0300

    feat: add crypto wallet balance UI

--- components/ui/dashboard/user-wallet.tsx
+++ components/ui/dashboard/user-wallet.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from 'next/image'
+import Link from 'next/link'
+import BTC_LOGO from '@/app/assets/logo/bitcoin-btc-logo.svg'
+import ETH_LOGO from '@/app/assets/logo/ethereum-eth-logo.svg'
+import SOL_lOGO from '@/app/assets/logo/solana-sol-logo.svg'
+import MATIC_LOGO from '@/app/assets/logo/polygon-matic-logo.svg'
+import USDT_LOGO from '@/app/assets/logo/crypto/usdt_transparent.png'
+import USDC_LOGO from '@/app/assets/logo/crypto/usdc_transparent.png'
+import cUSD_LOGO from '@/app/assets/logo/crypto/cUSD.png'
 
 export function UserWallet(){
     return (
@@ -10,6 +27,149 @@ export function UserWallet(){
                         <h2 className="text-3xl font-bold tracking-tight">
                             My Wallet
                         </h2>
+                    </div>
+                    <div className='grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+                        <Card className="col-span-4">
+                            <CardHeader>
+                                <div className="flex items-center justify-between space-y-2">
+                                    <div className="md:flex items-center space-x-2">
+                                    <CardTitle>
+                                        Crypto Wallet Balance
+                                    </CardTitle>
+
+                                    </div>
+                                </div>   
+                            </CardHeader>
+                            <CardContent className="pl-2">
+                                <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+                                    <Card>
+                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                        <CardTitle className="text-sm font-medium">
+                                            BTC
+                                        </CardTitle>
+                                        <Image
+                                            src={BTC_LOGO.src}
+                                            width={30}
+                                            height={30}
+                                            alt={'btc-logo'}
+                                        />
+                                        </CardHeader>
+                                        <CardContent>
+                                            <div className='text-2xl font-bold'>
+                                                KES 43,184.00
+                                            </div>
+                                            <p className="text-xs text-muted-foreground mb-6">
+                                                <span className="text-green-600">
+                                                    10.19%
+                                                </span> last 1 month
+                                            </p>
+                                            <Button
+                                                //className="w-full"
+                                            >
+                                                <Link href={`/dashboard/buy-sell-crypto?baseAsset=BTC&currency=KES`}>
+                                                    Buy/Sell
+                                                </Link>
+                                            </Button>
+                                        </CardContent>
+                                    </Card>
+
+                                    <Card>
+                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                        <CardTitle className="text-sm font-medium">
+                                            USDT
+                                        </CardTitle>
+                                        <Image
+                                            src={USDT_LOGO.src}
+                                            width={30}
+                                            height={30}
+                                            alt={'btc-logo'}
+                                        />
+                                        </CardHeader>
+                                        <CardContent>
+                                            <div className='text-2xl font-bold'>
+                                                KES 5,184.00
+                                            </div>
+                                            <p className="text-xs text-muted-foreground mb-6">
+                                                <span className="text-green-600">
+                                                    34.97%
+                                                </span> last 1 month
+                                            </p>
+                                            <Button
+                                                //className="w-full"
+                                            >
+                                                <Link href={`/dashboard/buy-sell-crypto?baseAsset=BTC&currency=KES`}>
+                                                    Buy/Sell
+                                                </Link>
+                                            </Button>
+                                        </CardContent>
+                                    </Card>
+
+                                    <Card>
+                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                        <CardTitle className="text-sm font-medium">
+                                            USDC
+                                        </CardTitle>
+                                        <Image
+                                            src={USDC_LOGO.src}
+                                            width={30}
+                                            height={30}
+                                            alt={'btc-logo'}
+                                        />
+                                        </CardHeader>
+                                        <CardContent>
+                                            <div className='text-2xl font-bold'>
+                                                KES 8,184.00
+                                            </div>
+                                            <p className="text-xs text-muted-foreground mb-6">
+                                                <span className="text-green-600">
+                                                    25.93%
+                                                </span> last 1 month
+                                            </p>
+                                            <Button
+                                                //className="w-full"
+                                            >
+                                                <Link href={`/dashboard/buy-sell-crypto?baseAsset=BTC&currency=KES`}>
+                                                    Buy/Sell
+                                                </Link>
+                                            </Button>
+                                        </CardContent>
+                                    </Card>
+
+                                    <Card>
+                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                        <CardTitle className="text-sm font-medium">
+                                            ETH
+                                        </CardTitle>
+                                        <Image
+                                            src={ETH_LOGO.src}
+                                            width={30}
+                                            height={30}
+                                            alt={'btc-logo'}
+                                        />
+                                        </CardHeader>
+                                        <CardContent>
+                                            <div className='text-2xl font-bold'>
+                                                KES 2,184.00
+                                            </div>
+                                            <p className="text-xs text-muted-foreground mb-6">
+                                                <span className="text-red-600">
+                                                    2.23%
+                                                </span> last 1 month
+                                            </p>
+                                            <Button
+                                                //className="w-full"
+                                            >
+                                                <Link href={`/dashboard/buy-sell-crypto?baseAsset=BTC&currency=KES`}>
+                                                    Buy/Sell
+                                                </Link>
+                                            </Button>
+                                        </CardContent>
+                                    </Card>
+
+
+                                </div>
+                            </CardContent>
+                        </Card>
                     </div>
                 </div>
             </ScrollArea>
