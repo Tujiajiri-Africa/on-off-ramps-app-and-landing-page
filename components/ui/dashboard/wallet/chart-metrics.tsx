9fde585cbc87624502c3db298b7df27cee74c8ae
dickensodera <dickens@laevitas.ch>
2024-04-01 06:10:44 +0300

    update: Add dashboard illustration on hero page

--- components/ui/dashboard/wallet/chart-metrics.tsx
+++ components/ui/dashboard/wallet/chart-metrics.tsx
@@ -1,7 +1,20 @@
 'use client'
 
 import React from 'react'
-import { BarChart, Bar, Rectangle, Area, AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, 
+  Bar, 
+  Rectangle, 
+  Area, 
+  AreaChart, 
+  XAxis, 
+  YAxis, 
+  CartesianGrid, 
+  Tooltip, 
+  Legend, 
+  ResponsiveContainer,
+  LineChart,
+  Line
+ } from 'recharts';
 
 import {sampleWalletData} from '@/helpers/data'
 
@@ -12,7 +25,7 @@ export function UserBalanceChartAnalysis(){
               //width={'99%'}
               //height={300}
               height={350}>
-                <AreaChart
+                {/* <AreaChart
                 width={500}
                 height={400}
                 data={sampleWalletData}
@@ -31,16 +44,38 @@ export function UserBalanceChartAnalysis(){
                     axisLine={false}
                 />
                 <YAxis 
-                  tickFormatter={(value) => `$${value}`}
+                  tickFormatter={(value) => `K${value}`}
                   fontSize={12}
                   tickLine={false}
                   axisLine={false}
                     />
                 <Tooltip />
-                <Area type="monotone" name="total" dataKey="total" stackId="1" stroke="#7DA0FA" fill="#7DA0FA" />
-                <Area type="monotone" name="incoming" dataKey="incoming" stackId="2" stroke="#4B49AC" fill="#4B49AC" />
-                <Area type="monotone" name="outgoing" dataKey="outgoing" stackId="3" stroke="#F3797E" fill="#F3797E" />
-                </AreaChart>
+                <Area type="monotone" name="total" dataKey="total" stackId="1" stroke="#ea580c" fill="#ea580c" />
+                <Area type="monotone" name="incoming" dataKey="incoming" stackId="2" stroke="#16a34a" fill="#16a34a" />
+                <Area type="monotone" name="outgoing" dataKey="outgoing" stackId="3" stroke="#4B49AC" fill="#4B49AC" />
+                </AreaChart> */}
+                <LineChart width={480} height={400} data={sampleWalletData}
+  margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+  <CartesianGrid strokeDasharray="3 3" />
+  <XAxis 
+                      dataKey="label" 
+                      fontSize={12}
+                      tickLine={false}
+                      axisLine={false}
+  //dataKey="label" 
+  />
+  <YAxis
+                    tickFormatter={(value) => `KES${value}`}
+                    fontSize={10}
+                    tickLine={false}
+                    axisLine={false}
+  />
+  <Tooltip />
+  <Legend />
+  <Line type="monotone" name="total" dataKey="total"  stroke="#ea580c" fill="#ea580c" />
+  <Line type="monotone" name="incoming" dataKey="incoming" stroke="#16a34a" fill="#16a34a" />
+  <Line type="monotone" name="outgoing" dataKey="outgoing"  stroke="#F00FDA" fill="#F00FDA" />
+</LineChart>
             </ResponsiveContainer>
     )
 }
