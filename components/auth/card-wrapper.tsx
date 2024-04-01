d02c67991d4a36b9cc51a9e8072fb6459438ce8a
dickensodera <dickens@laevitas.ch>
2024-04-01 07:07:11 +0300

    update: Modify auth pages

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -24,7 +24,10 @@ export const CardWrapper = ({
     }: CardWrapperProps)=>{
     return (
         <>
-        <div className='min-h-screen bg-gradient-to-r from-[#FDC707] to-[#F00FDA] flex flex-col justify-center py-12 sm:px-2 lg:px-8 border-none'>
+        <div className='min-h-screen bg-gradient-to-r from-green-50/50 via-teal-50 to-green-50/50 flex flex-col justify-center py-12 sm:px-2 lg:px-8 border-none'
+        //bg-gradient-to-r from-green-50/50 via-teal-50 to-green-50/50
+        //bg-gradient-to-r from-[#FDC707] to-[#F00FDA]
+        >
             <CardContent className="mt-8 sm:mx-auto sm:w-full sm:max-w-md space-y-4">
             <div className="w-[98%] sm:w-full mx-auto  bg-white py-8 px-4 shadow rounded-lg sm:rounded-lg sm:px-4">
             <CardHeader>
--- components/auth/login/login.tsx
+++ components/auth/login/login.tsx
@@ -153,7 +153,7 @@ export const LoginForm = () => {
                     <Button 
                         disabled={isPending}
                         type="submit"
-                        className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gradient-to-r from-[#FDC707] to-[#F00FDA] ">
+                        className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-500">
 
                         Sign in
                     </Button>
