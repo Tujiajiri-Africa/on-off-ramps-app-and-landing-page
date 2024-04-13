7e5d8e0d727cb3d0abe053a1eeaf5a39a83dce43
dickensodera <dickens@laevitas.ch>
2024-04-13 20:50:11 +0300

    feat: page pdf download

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,8 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import {supportedAssets} from '@/helpers/data'
 import SOL_lOGO from '@/app/assets/logo/solana-sol-logo.svg'
+//import jsPDF from "jspdf";
+//import html2canvas  from 'html2canvas'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -35,4 +37,25 @@ export const getAssetImage = (label:string) =>{
     return SOL_lOGO
   }
  return asset?.icon
+}
+
+//"jspdf-invoice-template": "^1.4.3",
+// "html2canvas": "^1.4.1",
+export const convertInvoiceToPdf = (data:any, file_name:string) =>{
+  //const doc = new jsPDF();
+
+  //doc.text(data, 10, 10); //doc.addPage()// 
+  //doc.save(`${file_name}.pdf`);
+}
+
+export const generatePdf = (content:any) =>{
+  const options = {
+    filename: 'my-document.pdf',
+    margin: 1,
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+  };
+
+  //html2pdf().set(options).from(content).save();
 }
