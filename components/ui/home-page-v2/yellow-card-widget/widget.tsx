24e41bbc7fa362c17f13132f12f236bd439eaebd
dickensodera <dickens@laevitas.ch>
2024-04-24 11:14:03 +0300

    update: optimized widget for mobile view

--- components/ui/home-page-v2/yellow-card-widget/widget.tsx
+++ components/ui/home-page-v2/yellow-card-widget/widget.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card, CardHeader, CardTitle, CardContent, CardDescription } from '@/components/ui/card'
+import BackgroundIntroImage from '@/app/assets/backgrounds/auth/intro-page-image-1-svg.svg'
 
 export function YCWidget(){
     const API_KEY = process.env.YC_WIDGET_SANDBOX_API_KEY
@@ -7,23 +8,31 @@ export function YCWidget(){
     const SANDBOX_BASE_URL = process.env.YC_WIDGET_SANDBOX_BASE_URL
 
     return (
+      <>
         <div
-        className="flex  w-full h-full 2xl:h-screen flex-col items-center justify-center text-center px-4 py-20 bg-gradient-to-r from-green-50/50 via-teal-50 to-green-50/50 dark:from-gray-800 dark:via-gray-700 dark:to-gray-800">
-          <div className="flex justify-center bg-transparent shadow-2xl rounded-md"
-            >
+        className="YCWidgetContainer flex h-full 2xl:min-h-screen flex-col items-center justify-center text-center px-4 py-20 bg-gradient-to-r from-green-50/50 via-teal-50 to-green-50/50 dark:from-gray-800 dark:via-gray-700 dark:to-gray-800"
+        >
+          <div className="flex justify-center bg-transparent shadow-2xl rounded-md" 
+            id="YCWidget">
                 <iframe
-                  //src="https://pagedone.io/asset/uploads/1691054543.png"
                   src={`${SANDBOX_BASE_URL}/${API_KEY}`}
-                  width={1024}
-                  height={800}
+                  
+                  
                   style={{
-                    overflow: "hidden"
+                    overflow: "hidden",
+                    width: 800,
+                    height: 900,
+                    padding: 0,
+                    margin: 'auto'
                   }}
                   
-                  className="rounded-md overflow-hidden "
+                  className="rounded-md overflow-hidden mx-auto"
                 />
               </div>
       </div>
+      </>
+
+
     )
 }
 
