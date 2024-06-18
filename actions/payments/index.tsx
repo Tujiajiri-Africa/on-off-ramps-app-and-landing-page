f23245a33449d694596c98f2b60241826e9c5f2c
dickensodera <dickens@laevitas.ch>
2024-06-18 20:44:25 +0300

    update: add wallet functionalities

---
+++ actions/payments/index.tsx
@@ -0,0 +1,35 @@
+
+export const depositFiat = async() => {
+
+}
+
+export const fetchUserFiatBalance = async() =>{
+
+}
+
+export const buyCrypto = async() =>{
+
+}
+
+export const sellCrypto = async() =>{
+
+}
+
+export const sendCrypto = async() =>{
+
+}
+
+export const receiveCrypto = async() =>{
+
+}
+
+export const sendFiat = async() =>{
+
+}
+
+export const receiveFiat = async() =>{
+    
+}
+
+
+
