3018f1e8cefcc211a055df1b90f7b389ee5488dd
dickensodera <dickens@laevitas.ch>
2024-06-18 20:54:36 +0300

    update: fetch transaction history

--- actions/payments/index.tsx
+++ actions/payments/index.tsx
@@ -1,10 +1,56 @@
+"use server"
+
+import  * as z from 'zod'
+import { DEV_BASE_URI, PROD_BASE_URI, ENVIRONMENT, TransactionHistoryProps } from '@/helpers/data'
 
 export const depositFiat = async() => {
 
 }
 
-export const fetchUserFiatBalance = async() =>{
+export const fetchTransactionHistory = async(bearerToken: string|undefined) =>{
+    const endpoint = ENVIRONMENT == 'local' ? DEV_BASE_URI + '/transactions/history' : PROD_BASE_URI + '/transactions/history'
 
+    let transactions: TransactionHistoryProps[] = []
+   
+    const payload = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${bearerToken}`
+        }
+    }
+
+    const fetchUserTransactionHistory = await fetch(endpoint, payload).then(async(response) => {
+        if(response.status === 500){
+            transactions = []
+            return transactions
+        }
+
+        const data = await response.json()
+
+        if(data['status'] == false){
+            transactions = []
+            return transactions
+        }
+
+        if(data['status'] == true){
+            transactions = data['data']
+            return transactions
+        }
+    }).catch((error) =>{
+        console.log(error)
+        transactions = []
+        return transactions
+    })
+
+    try{
+        return fetchUserTransactionHistory
+    }catch(error){
+        console.log(error)
+        transactions = []
+        return transactions
+    }
 }
 
 export const buyCrypto = async() =>{
