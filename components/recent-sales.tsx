08b551a1e05fdfcb7a2afa20d501d8720ffabb2f
dickensodera <dickens@laevitas.ch>
2024-07-11 10:45:50 +0300

    update: Update image size on null transactions illustration

--- components/recent-sales.tsx
+++ components/recent-sales.tsx
@@ -123,8 +123,8 @@ const setStatusColor = (status: string|undefined) => {
           <Image 
             //src={InvoiceVector3.src} 
             src={ItmesNotFoundIllustration.src}
-            height={200} 
-            width={200} //320
+            height={100} 
+            width={100} //320
             // height={320} 
             // width={320} //320
             alt="invoice-vector-2" 
