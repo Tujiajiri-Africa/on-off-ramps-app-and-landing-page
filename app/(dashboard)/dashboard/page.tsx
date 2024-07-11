bcf3b58c692372689fac4544087322de9564b590
dickensodera <dickens@laevitas.ch>
2024-07-11 07:16:17 +0300

    update: Update main dashboard content

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import {MainUserDashboard} from '@/components/ui/dashboard/index'
+import { UserWallet } from '@/components/ui/dashboard/user-wallet';
 
 export default async function Page() {
   return (
       <>
         <MainUserDashboard />
+
+        {/* <UserWallet /> */}
       </>
   );
 }
