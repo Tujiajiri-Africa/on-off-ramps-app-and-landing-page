a2728be89601693a850578277b419b3d9ef2bcad
dickensodera <dickens@laevitas.ch>
2024-07-12 18:44:38 +0300

    update: Update secondary statement on main user dashboard

--- components/ui/dashboard/getting-started/index.tsx
+++ components/ui/dashboard/getting-started/index.tsx
@@ -51,7 +51,8 @@ export function DashboardIntro(){
               </p> */}
                 <p className="text-md text-white mb-8">
                   {/* Start buying and selling crypto, create and manage invoices with ease. */}
-                    Start accumulating rewards in cUSD and {userSessionData?.user.currency}. When you buy and sell cUSD from us via MiniPay, you get a 2% cashback for every cUSD purchase, claimable after every 7 days.
+                  {/* Start accumulating rewards in cUSD and {userSessionData?.user.currency}.  */}
+                    When you buy and sell cUSD from us via MiniPay, you get a 2% cashback for every cUSD purchase, claimable after every 7 days.
                   {/* Get cash rewards for trading cUSD with M-Pesa on MiniPay */}
               </p>
               <Link href="/dashboard/buy-sell-crypto" className="bg-white hover:bg-gray-200 text-purple-600 font-bold py-2 px-4 rounded">
