ca909e1dbdf71c68a2bfb5a078ac34bfe61ce61b
dickensodera <dickens@laevitas.ch>
2024-07-12 09:45:56 +0300

    update: remove left margin on second div of transaction history

--- components/recent-sales.tsx
+++ components/recent-sales.tsx
@@ -205,7 +205,7 @@ const setStatusColor = (status: string|undefined) => {
               {transansactionItem.status}
             </p>
           </div>
-          <div className="ml-4 space-y-1"
+          <div className="space-y-1"
             //items-start
           >
               <p className='text-md font-medium leading-none'>{transansactionItem.asset_name} {transansactionItem.amount}</p>
