8188c39ec9e25a45a492d9405c96a92cc3e93c42
dickensodera <dickens@laevitas.ch>
2024-07-13 01:28:36 +0300

    update: Add link on transactions on main dashboard

--- components/ui/dashboard/index.tsx
+++ components/ui/dashboard/index.tsx
@@ -416,14 +416,15 @@ export function MainUserDashboard() {
                                         <CardTitle>Recent Transactions</CardTitle>
                                         
                                         </div>
-                                        <Button 
+                                        <Link 
+                                        href={'/dashboard/transactions'}
                                             className="content-start group  rounded-md px-3 py-2 text-sm bg-orange-600 text-white font-medium hover:bg-orange-500 hover:text-white"
-                                            variant={'outline'}
+                                            //variant={'outline'}
                                         >
                                           <div className='flex items-center gap-2'>
                                                 View More <ArrowUpRight className='h-6 w-6' />
                                           </div>
-                                        </Button>
+                                        </Link>
                                     </div> 
                       
                       <CardDescription>
