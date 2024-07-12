8a4cb219b342b919b559e0864d0f6c3b0c8222bd
dickensodera <dickens@laevitas.ch>
2024-07-13 00:42:11 +0300

    update: trigger redeployment

--- components/ui/dashboard/rewards/index.tsx
+++ components/ui/dashboard/rewards/index.tsx
@@ -45,11 +45,6 @@ export function UserRewardInfoPage(){
             
             >
                 <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
-                    {/* <div className="flex items-center justify-between space-y-2">
-                        <h2 className="text-3xl font-bold tracking-tight">
-                            My Wallet
-                        </h2>
-                    </div> */}
                     <div 
                         className='grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-2'
                     //className='grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-7'
