d012b8c1af6d48b771550e157676f702ab119aa8
dickensodera <dickens@laevitas.ch>
2024-07-12 08:53:18 +0300

    update: add room for nullable decimals

--- components/recent-sales.tsx
+++ components/recent-sales.tsx
@@ -148,7 +148,7 @@ const setStatusColor = (status: string|undefined) => {
       </div>
       
       : 
-      transansactionHistoryData && transansactionHistoryData.length > 1 && transansactionHistoryData?.slice(0, 5).map((transansactionItem, index) => (
+      transansactionHistoryData && transansactionHistoryData.length > 1 && transansactionHistoryData?.slice(0, 10).map((transansactionItem, index) => (
           <div key={index} className="flex items-center">
           <Avatar className="h-9 w-9">
             <AvatarImage src="/avatars/01.png" alt="Avatar" />
@@ -165,7 +165,7 @@ const setStatusColor = (status: string|undefined) => {
             </p>
           </div>
           <div className="ml-auto font-medium">
-              <p>{transansactionItem.asset_name} {transansactionItem.amount.toPrecision(4)}</p>
+              <p>{transansactionItem.asset_name} {transansactionItem.amount?.toPrecision(4)}</p>
               <p className='text-sm'>{transansactionItem.created_at}</p>
             </div>
         </div>
