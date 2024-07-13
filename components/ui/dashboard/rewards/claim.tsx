fea6570689397f6e4faf7c421da82abb858882be
dickensodera <dickens@laevitas.ch>
2024-07-13 03:46:24 +0300

    update: revert type to number

--- components/ui/dashboard/rewards/claim.tsx
+++ components/ui/dashboard/rewards/claim.tsx
@@ -133,9 +133,9 @@ export function RewardClaimsForm(){
                                                 {...field}
                                                 //className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
                                                 placeholder="Enter cUSD amount"
-                                                type='text'
+                                                type='number'
                                                 disabled={isPending}
-                                                //min={0}
+                                                min={0}
                                                 onChangeCapture={e => handleInputAmountChange(e.currentTarget.value)}
                                             />
                                         </FormControl>
