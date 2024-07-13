a9e30637132217e033a48a9ed9b24f69f9177987
dickensodera <dickens@laevitas.ch>
2024-07-13 19:47:25 +0300

    update: Added payments component

--- app/(dashboard)/dashboard/pay/page.tsx
+++ app/(dashboard)/dashboard/pay/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import {RewardClaimsForm} from '@/components/ui/dashboard/rewards/claim'
+import { PaymentComponent } from '@/components/ui/dashboard/payments'
 
 export default function Pay(){
     return (<>
-        <RewardClaimsForm />
+        <PaymentComponent />
     </>
 )
 }
