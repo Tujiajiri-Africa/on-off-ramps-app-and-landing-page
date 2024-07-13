ea858be899f947414ed713f6d3078cf2fc5c177e
dickensodera <dickens@laevitas.ch>
2024-07-13 07:38:52 +0300

    update: Increase border bottom for balance on claim and withdraw page

--- components/ui/dashboard/withdraw.tsx
+++ components/ui/dashboard/withdraw.tsx
@@ -126,7 +126,7 @@ export function WithdrawForm(){
                                     <div 
                                         className='mt-1'
                                         >
-                                        <FormControl>
+                                        <FormControl className='mb-2'>
                                             <Input
                                                 {...field}
                                                 //className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
