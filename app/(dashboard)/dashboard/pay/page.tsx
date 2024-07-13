2a0df781a8e879d3a55293e034fc17eb9efe92a0
dickensodera <dickens@laevitas.ch>
2024-07-13 22:25:44 +0300

    update: Remove unused component to optmize page

--- app/(dashboard)/dashboard/pay/page.tsx
+++ app/(dashboard)/dashboard/pay/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import {RewardClaimsForm} from '@/components/ui/dashboard/rewards/claim'
 import { PaymentComponent } from '@/components/ui/dashboard/payments'
 
 export default function Pay(){
