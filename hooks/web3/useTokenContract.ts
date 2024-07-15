fd58270373c5237c8373ed828d4f8159f34f87ad
dickensodera <dickens@laevitas.ch>
2024-07-15 08:41:07 +0300

    update: send transaction via web3

--- hooks/web3/useTokenContract.ts
+++ hooks/web3/useTokenContract.ts
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { useAccount, useNetwork } from 'wagmi'
 import {CELO_MAINNET_CHAIN_ID, cUSD_MAINNET_CONTRACT_ADDRESS} from '@/helpers/data'
 import CUSD_TOKEN_CONTRACT_ABI from '@/lib/artifacts/cUSD_contract_abi'
-import {} from 'usehooks-ts'
 
 export const useTokenContract = () => {
     const {isConnected, address} = useAccount()
@@ -12,9 +11,10 @@ export const useTokenContract = () => {
     const [activeTokenContractAbi, setActiveTokenContractAbi] = useState<any>();
     
     useEffect(() => {
-        if(!chain?.id){return;}
+        if(!address || !isConnected) return;
 
-       if(!address || !isConnected) return;
+        if(!chain?.id){return;}
+        
         switch(chain.id){
             case CELO_MAINNET_CHAIN_ID:
               setTokenAddress(cUSD_MAINNET_CONTRACT_ADDRESS)
